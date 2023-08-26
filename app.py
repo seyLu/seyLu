2870cb537078e6804339a475afeef38ec6810eac
seylu <98249191+seyLu@users.noreply.github.com>
2023-08-26 13:43:17 +0800

    chore: lint

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 from flask import Flask, render_template
 
-from markdown import markdown # type: ignore[attr-defined]
+from markdown import markdown  # type: ignore[attr-defined]
 
 app = Flask(__name__)
 
